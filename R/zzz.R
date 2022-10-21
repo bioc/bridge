f99ab8e35741c24b9012a8d33d29d53d42c6550a
lshep <lori.shepherd@roswellpark.org>
2022-10-21 11:17:29 -0400

    Deprecate
     - unresponsive

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,9 @@
 .onLoad<-function(lib,pkg){
     require("rama",  quietly=TRUE) || stop("rama package not found")
 }
+.onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.17")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+}
